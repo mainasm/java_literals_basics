ae62be6bf112003e97d6e09eb0afc2e91df6b7db
mainasm <maina.sam@outlook.com>
2022-09-23 04:00:33 +0300

    Added string literals

---
+++ src/JavaStringLiterals.java
@@ -0,0 +1,10 @@
+public class JavaStringLiterals {
+    public static void main(String[] args) {
+        String myString = "I am a string literal";
+        System.out.println("Who I'm I? \n" + myString);
+        System.out.println("What is my length? ");
+
+        //String method length() get length in characters
+        System.out.println("I am " + myString.length()
+                + " characters long");    }
+}
